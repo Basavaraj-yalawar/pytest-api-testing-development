998bc1bb50be709eff63ce901323b44d51624379
basavaraj.yalawar <basavaraj.yalawar@hymdl.io>
2026-01-20 16:09:54 +0530

    Initial commit: pytest web automation framework

---
+++ tests/test_run_test.py
@@ -0,0 +1,8 @@
+from engine.run_test import run_test
+
+def test_run_test():
+    run_test(
+        base_url="https://practicetestautomation.com/practice-test-login/",
+        username="student",
+        password="Password123"
+    )
